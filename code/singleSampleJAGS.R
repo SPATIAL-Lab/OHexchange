482e851e65272fdf00f68b9e2a93525f6c6c7522
Gabriel Bowen <gabe.bowen@utah.edu>
2025-05-22 11:05:04 -0600

    Add ssDriver

--- code/singleSampleJAGS.R
+++ code/singleSampleJAGS.R
@@ -4,9 +4,9 @@ model {
   
   # Prior parameter estimates
   ## True d18O of tooth enamel carbonate
-  d18O_t ~ dnorm(d18_ut.m, 1 / d18O_ut.sd ^ 2)
+  d18O_t ~ dnorm(d18O_ut[1], 1 / d18O_ut[2] ^ 2)
   ## Fractionation factor between water and enamel hydroxyl
-  a_ex ~ dunif(0.990, 1.1)
+  a_ex ~ dunif(0.990, 1.01)
   ## Fraction of measured d18O that derives from water
   f_ex ~ dunif(0, 0.3)
   
@@ -14,6 +14,5 @@ model {
   d18O_p = (1 - f_ex) * d18O_t + f_ex * a_ex * (d18O_w + (a_ex - 1) * 1000)
   
   # Likelihood
-  d18O_m ~ dnorm(d18O_p, 1 / d18O_m.sd ^ 2)
-
+  d18O_m[1] ~ dnorm(d18O_p, 1 / d18O_m[2] ^ 2)
 }
---
+++ code/ssDriver.R
@@ -0,0 +1,36 @@
+library(R2jags)
+
+# Read in datasets
+treat = read.csv("data/BRT1-T7_treatments.csv")
+result = read.csv("data/BRT1-T7results.csv")
+
+# Untreated (aka true) enamel d18O, w/ made up SD for now
+d18O_ut = result$d18O[result$Tooth.ID == "BR1" & result$Treatment.Number == 0]
+d18O_ut = c(d18O_ut, 0.3)
+
+# Measured enamel d18O and made up SD
+d18O_m = result$d18O[result$Tooth.ID == "BR1" & result$Treatment.Number == 5]
+d18O_m = c(d18O_m, 0.3)
+
+# Treatment water d18O
+d18O_w = treat$Rinse.d18O[treat$Treatment.Number == 5]
+
+# Bundle for JAGS
+d = list(d18O_ut = d18O_ut, d18O_m = d18O_m, d18O_w = d18O_w)
+
+# Parameters to save
+p = c("d18O_t", "a_ex", "f_ex", "d18O_p")
+
+# Run the JAGS analysis
+post = jags(d, NULL, p, "code/singleSampleJAGS.R")
+
+# View the summary results and traceplots
+View(post$BUGSoutput$summary)
+traceplot(post)
+
+# Look at some of the posterior distributions
+plot(density(post$BUGSoutput$sims.list$f_ex))
+plot(density(post$BUGSoutput$sims.list$a_ex))
+
+# Look at bivariate parameter space
+plot(post$BUGSoutput$sims.list$a_ex, post$BUGSoutput$sims.list$f_ex)
