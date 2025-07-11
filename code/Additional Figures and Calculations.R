6b4ea61bcadc3c847c784653f02eb9f1d6398701
Ben Rivera <benrivera1820@gmail.com>
2025-07-11 14:59:52 -0600

    Making another figure to try to sort some things out.

--- code/Additional Figures and Calculations.R
+++ code/Additional Figures and Calculations.R
@@ -52,92 +52,50 @@ legend("bottomright",
        xpd = NA,
        cex = 0.9)
 
+# Rinse d18O vs D18O
+mmat <- rbind(t1diff, t2diff, t3diff) #first getting the averages for each tooth for each treatment with each rinse
+lmat <- rbind(t4diff, t6diff)
+mr_avg <- colMeans(mmat)
+lr_avg <- colMeans(lmat)
 
-#Plot with just treatments 1-4 and the T4 redo
-
-data_list <- list(t1diff, t2diff, t3diff, t4diff, t4rdiff)
-par(mar = c(5, 7, 4, 9))
-boxplot(data_list, xaxt = "n",
-        main = expression("T1 through T4 with T4 redo"), 
-        ylab = expression("Δ"^18 * "O (‰)"), 
-        xlab = "Treatment Number",
-        ylim = c(-0.25, 1.75),
-        col = c("lightblue", "salmon", "#F6E5C3", "salmon", "salmon"),
-        border = c("#841618", "#841618", "#841618", "#297770", "#297770"))
-# Dotted Line
-abline(h = 0, col = "black", lty = 2)
-#X-axis markers
-axis(1, at = 1:5, labels = c("1", "2", "3", "4", "4 redo"))
-# First Legend
-legend("topright", 
-       legend = c("Light Acid", "Medium Acid", "Heavy Acid"),
-       fill = c("lightblue", "salmon", "#F6E5C3"),          
-       bty = "y",                   
-       title = "Fill",          
-       inset = c(-0.4, 0),
-       xpd = NA) 
-# Second Legend
-legend("right",
-       legend = c("Light Rinse", "Medium Rinse", "Heavy Rinse"),
-       fill = c("#297770", "#841618", "#8E5722"),
-       bty = "y",
-       title = "Outline",
-       inset = c(-0.425, 0),
-       xpd = NA)
-
-#T4 vs T4R vs T4A
-
-data_list <- list(t4diff, t4rdiff, t4adiff)
-par(mar = c(5, 7, 4, 9))
-boxplot(data_list, xaxt = "n",
-        main = expression("Original T4, T4 Redo, and T4 Average"), 
-        ylab = expression("Δ"^18 * "O (‰)"), 
-        xlab = "Treatment Number",
-        ylim = c(-0.25, 1.75),
-        col = c("salmon", "salmon", "salmon"),
-        border = c("#297770","#297770","#297770"))
-# Dotted Line
-abline(h = 0, col = "black", lty = 2)
-#X-axis markers
-axis(1, at = 1:3, labels = c("T4", "T4 redo", "T4 avg"))
-# First Legend
-legend("topright", 
-       legend = c("Light Acid", "Medium Acid", "Heavy Acid"),
-       fill = c("lightblue", "salmon", "#F6E5C3"),          
-       bty = "y",                   
-       title = "Fill",          
-       inset = c(-0.4, 0),
-       xpd = NA) 
-# Second Legend
-legend("right",
-       legend = c("Light Rinse", "Medium Rinse", "Heavy Rinse"),
-       fill = c("#297770", "#841618", "#8E5722"),
-       bty = "y",
-       title = "Outline",
-       inset = c(-0.425, 0),
-       xpd = NA)
+data_list3 <- list(lr_avg, mr_avg, t7diff, t5diff)
+boxplot(data_list3, at = x_positions, names = x_positions,
+        boxwex = 20,
+        xaxt = "n",
+        xlim = c(-50, 260),
+        xlab = "Rinse d18O",
+        ylab = "Isotope shift (permil)",
+        main = "Correlation Between Rinse and Shift Magnitude")
+axis(side = 1, at = seq(-45, 260, by = 25))
+abline(v = 0, lty = 2, col = "black")
 
 # Figure to show lack of correlation between D18O and untreated enamel value
 # Big Delta vs Little Delta
 # Increase margins for legend
 par(mar = c(6, 4, 4, 8))
 # Make plot
-plot(teethdata$UT.Data, -1* (teethdata$UT.T5), col = "orange", pch = 19, 
-     xlab = expression("δ"^18 * "O of Control Teeth (‰)"), ylab = expression("Δ"^18 * "O (‰)"), main = expression("Differences in δ"^18 * "O between Control, Treatments"), ylim = c(0.2, 5))
-points(teethdata$UT.Data, -1* (teethdata$UT.T2), col = "red", pch = 19)
-points(teethdata$UT.Data, -1* (teethdata$UT.T3), col = "blue", pch = 19)
-points(teethdata$UT.Data, -1* (teethdata$UT.T4), col = "purple", pch = 19)
-points(teethdata$UT.Data, -1* (teethdata$UT.T1), col = "green", pch = 19)
-points(teethdata$UT.Data, -1* (teethdata$UT.T6), col = "black", pch = 19)
-points(teethdata$UT.Data, -1* (teethdata$UT.T7), col = "pink", pch = 19)
+par(mar = c(5, 5, 5, 7))
+plot(teethdata$UT.Data, -1* (teethdata$UT.T5), col = "salmon", 
+     pch = 19, 
+     xlab = expression("δ"^18 * "O of Control Teeth (‰)"), 
+     ylab = expression("Δ"^18 * "O (‰)"), 
+     main = expression("Differences in δ"^18 * "O between Control, Treatments"), 
+     ylim = c(0.2, 5))
+points(teethdata$UT.Data, -1* (teethdata$UT.T2), col = "#7eb0d5", pch = 19)
+points(teethdata$UT.Data, -1* (teethdata$UT.T3), col = "#297770", pch = 19)
+points(teethdata$UT.Data, -1* (teethdata$UT.T4), col = "#841618", pch = 19)
+points(teethdata$UT.Data, -1* (teethdata$UT.T1), col = "#ffb400", pch = 19)
+points(teethdata$UT.Data, -1* (teethdata$UT.T6), col = "#776bcd", pch = 19)
+points(teethdata$UT.Data, -1* (teethdata$UT.T7), col = "#b2e061", pch = 19)
 # Legend
 legend("topright",
-       legend = c("Treatment 1", "Treatment 2", "Treatment 3", "Treatment 4", "Treatment 5"),
-       fill = c("green", "red", "blue", "purple", "orange"),
+       legend = c("Treatment 1", "Treatment 2", "Treatment 3", "Treatment 4", "Treatment 5", "Treatment 6", "Treatment 7"),
+       fill = c("#ffb400", "#7eb0d5", "#297770", "#841618", "salmon", "#776bcd", "#b2e061"),
        bty = "y",
        title = "Legend",
-       inset = c(-0.33, 0),
-       xpd = NA)
+       inset = c(-0.425, 0),
+       xpd = NA,
+       cex = 0.9)
 
 
 #anova testing
